27d4045c7e3df6d9e7713ef4199dee1bb4f56f39
Felice <Felixe70@gmail.com>
2023-02-18 12:42:06 +0100

    Conditional Rendering 4

---
+++ 18. Conditional Rendering 4/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <>
+      -----------------
+      <Welcome name="john" age="18" />
+      -----------------
+      <Welcome name="john" age="17" />
+    </>
+  );
+}
+
+export default App;
---
+++ 18. Conditional Rendering 4/src/Welcome.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Age from './Age'
+
+class Welcome extends React.Component{
+    render(){
+        return(
+            <>
+                {this.props.name=="john" && <p>Welcome , {this.props.name}</p>}
+                {this.props.age>=18 && this.props.age<=65 && <Age age={this.props.age}/>}
+            </>
+        )
+    }
+    static defaultProps={
+        name:"name is not defined",
+        age:"age is not defined"
+    }
+}
+
+export default Welcome;
