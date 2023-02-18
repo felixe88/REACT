3748d5aeab8a716a75396a259bddcd9e9b33aac1
Felice <Felixe70@gmail.com>
2023-02-18 12:45:56 +0100

    Conditional Rendering 5

---
+++ 19. Conditional Rendering 5/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <>
+      <Welcome name="Felice" age="18" />
+      ---------------
+      <Welcome name="Felice" age="17" />
+    </>
+  );
+}
+
+export default App;
