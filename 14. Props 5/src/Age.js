669e67323d0e20393e1ab103ee55054893378bd6
Felice <Felixe70@gmail.com>
2023-02-18 12:29:56 +0100

    Props 5

---
+++ 14. Props 5/src/Age.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class age extends React.Component{
+    render(){
+        return(
+            <p>Your age is: {this.props.age}</p>
+        )
+    }
+}
+export default age
---
+++ 14. Props 5/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <Welcome name={<strong>Felice</strong>} age="21"/>
+  );
+}
+
+export default App;
+
